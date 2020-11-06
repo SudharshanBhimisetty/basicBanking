f6be16c781cb36ea7e0cc40098bb2255a9bb96f4
SudharshanBhimisetty <sudharshanbhimisetty24@gmail.com>
2020-11-06 16:05:17 +0530

    restructuring of files is done

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <title>Basic Banking System</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-    <link href="index.css" rel="stylesheet">
+    <link href="styles/index.css" rel="stylesheet">
 </head>
 <body>
     <h1 class="header">XYZ Bank </h1>
